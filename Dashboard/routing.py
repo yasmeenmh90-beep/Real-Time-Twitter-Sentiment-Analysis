e07c4766ec881aef5b21b62dd2e9741185761ff6
Yasmeen_Azmat <yasmeenmh.90@gmail.com>
2026-02-02 02:11:23 +0400

    Delete Dashboard directory

--- Dashboard/routing.py
+++
@@ -1,8 +0,0 @@
-# dashboard/websockets/routing.py
-
-from django.urls import re_path
-from dashboard.websockets.consumers import SentimentConsumer
-
-websocket_urlpatterns = [
-    re_path(r"ws/sentiment/?$", SentimentConsumer.as_asgi()),
-]
